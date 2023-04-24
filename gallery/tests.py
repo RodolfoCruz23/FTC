85fe40fb4421795cb31286f73352481b8b82ef29
rcruz_23 <rcruz89@gmail.com>
2023-04-24 14:06:04 -0600

    Tests

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from django.urls import reverse
+from gallery.models import Gallery
 
-# Create your tests here.
+
+class GalleryTests(TestCase):
+    def setUp(self):
+        self.gallery = Gallery.objects.create(
+            name='Test Gallery',
+            pages=10
+        )
+
+    def test_gallery_list_view(self):
+        response = self.client.get(reverse('gallery_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.gallery.name)
+
+    def test_gallery_detail_view(self):
+        response = self.client.get(reverse('gallery_view', kwargs={'pk': self.gallery.pk}))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.gallery.name)
+
+    def test_gallery_create_view(self):
+        data = {
+            'name': 'New Gallery',
+            'pages': 5
+        }
+        response = self.client.post(reverse('gallery_new'), data=data)
+        self.assertEqual(response.status_code, 302)
+        gallery = Gallery.objects.get(name='New Gallery')
+        self.assertEqual(gallery.pages, 5)
+
+    def test_gallery_update_view(self):
+        data = {
+            'name': 'Updated Gallery',
+            'pages': 15
+        }
+        response = self.client.post(reverse('gallery_edit', kwargs={'pk': self.gallery.pk}), data=data)
+        self.assertEqual(response.status_code, 302)
+        gallery = Gallery.objects.get(pk=self.gallery.pk)
+        self.assertEqual(gallery.name, 'Updated Gallery')
+        self.assertEqual(gallery.pages, 15)
+
+    def test_gallery_delete_view(self):
+        response = self.client.post(reverse('gallery_delete', kwargs={'pk': self.gallery.pk}))
+        self.assertEqual(response.status_code, 302)
+        with self.assertRaises(Gallery.DoesNotExist):
+            Gallery.objects.get(pk=self.gallery.pk)
--- personal/tests.py
+++ personal/tests.py
@@ -1,3 +1,139 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Instructor, PersonalizedWorkout, Trainer
 
-# Create your tests here.
+class PersonalTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.instructor = Instructor.objects.create(
+            name='John Doe',
+            bio='Test Bio',
+            certifications='Test Certifications',
+            experience_years=5
+        )
+        self.workout = PersonalizedWorkout.objects.create(
+            name='Test Workout',
+            description='Test Description',
+            price=10.99
+        )
+        self.trainer = Trainer.objects.create(
+            name='Test Trainer',
+            bio='Test Bio',
+            certifications='Test Certifications',
+            experience_years=5
+        )
+        self.trainer.personalized_workouts.add(self.workout)
+
+    def test_instructor_list_view(self):
+        response = self.client.get(reverse('personal:instructor_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'personal/instructor_list.html')
+        self.assertContains(response, self.instructor.name)
+
+    def test_instructor_detail_view(self):
+        response = self.client.get(reverse('personal:instructor_detail', args=[self.instructor.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'personal/instructor_detail.html')
+        self.assertContains(response, self.instructor.name)
+
+    def test_custom_workout_view(self):
+        response = self.client.get(reverse('personal:custom_workout'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'personal/custom_workout.html')
+
+class InstructorModelTest(TestCase):
+
+    def setUp(self):
+        self.instructor = Instructor.objects.create(
+            name='John Doe',
+            bio='Fitness instructor with 10 years of experience',
+            certifications='ACE, NASM',
+            experience_years=10
+        )
+
+    def test_str(self):
+        self.assertEqual(str(self.instructor), 'John Doe')
+
+
+class PersonalizedWorkoutModelTest(TestCase):
+
+    def setUp(self):
+        self.workout = PersonalizedWorkout.objects.create(
+            name='Weightlifting 101',
+            description='Learn the basics of weightlifting',
+            price=29.99
+        )
+
+    def test_str(self):
+        self.assertEqual(str(self.workout), 'Weightlifting 101')
+
+
+class TrainerModelTest(TestCase):
+
+    def setUp(self):
+        self.trainer = Trainer.objects.create(
+            name='Jane Smith',
+            bio='Personal trainer with a passion for helping clients achieve their fitness goals',
+            certifications='NASM, ACE',
+            experience_years=5
+        )
+
+    def test_str(self):
+        self.assertEqual(str(self.trainer), 'Jane Smith')
+
+    def test_personalized_workouts(self):
+        workout = PersonalizedWorkout.objects.create(
+            name='Circuit Training',
+            description='A high-intensity workout that combines strength training and cardio',
+            price=39.99
+        )
+        self.trainer.personalized_workouts.add(workout)
+        self.assertIn(workout, self.trainer.personalized_workouts.all())
+
+
+class InstructorListViewTest(TestCase):
+
+    def setUp(self):
+        self.url = reverse('personal:instructor_list')
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'personal/instructor_list.html')
+
+
+class InstructorDetailViewTest(TestCase):
+
+    def setUp(self):
+        self.instructor = Instructor.objects.create(
+            name='John Doe',
+            bio='Fitness instructor with 10 years of experience',
+            certifications='ACE, NASM',
+            experience_years=10
+        )
+        self.url = reverse('personal:instructor_detail', args=[self.instructor.pk])
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'personal/instructor_detail.html')
+
+
+class CustomWorkoutViewTest(TestCase):
+
+    def setUp(self):
+        self.url = reverse('personal:custom_workout')
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'personal/custom_workout.html')
