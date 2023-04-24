3e60acfa9299e9dae0be790e08dce90a377780ed
rcruz_23 <rcruz89@gmail.com>
2023-04-24 15:24:06 -0600

    Gallery display

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from .views import GalleryDisplayView
 
 urlpatterns = [
     path('', views.GalleryList.as_view(), name='gallery_list'),
@@ -9,4 +10,5 @@ urlpatterns = [
     path('view/<int:pk>', views.GalleryView.as_view(), name='gallery_view'),
     path('edit/<int:pk>', views.GalleryUpdate.as_view(), name='gallery_edit'),
     path('delete/<int:pk>', views.GalleryDelete.as_view(), name='gallery_delete'),
+    path('display/', views.gallery_display, name='gallery_display'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-
+from django.shortcuts import render
 from gallery.models import Gallery
 
 class GalleryList(ListView):
@@ -24,5 +24,18 @@ class GalleryUpdate(UpdateView):
 class GalleryDelete(DeleteView):
     model = Gallery
     success_url = reverse_lazy('gallery_list')
+
+class GalleryDisplayView(TemplateView):
+    template_name = 'gallery_display.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['object_list'] = Gallery.objects.all()
+        return context
+
+def gallery_display(request):
+    galleries = Gallery.objects.all()
+    context = {'object_list': galleries}
+    return render(request, 'gallery_display.html', context)
 
 
